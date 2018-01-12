236ec24f842a5d638b1167ebe8af4d112eaf4130
Esora <tygyh2012@gmail.com>
2018-01-12 20:54:57 +0800

    update..

--- assets/js/index.js
+++ assets/js/index.js
@@ -27,9 +27,7 @@ var monsterAcceleration = 0.004;
 var malusClearColor = 0xb44b39;
 var malusClearAlpha = 0;
 
-var mousePos = { x: 0, y: 0 },
-    oldMousePos = {x:0, y: 0},
-    ballWallDepth = 28;
+var mousePos = { x: 0, y: 0 };
 
 // Materials
 var blackMat = new THREE.MeshPhongMaterial({
@@ -103,7 +101,7 @@ function createScene() {
 
     camera.position.x = 0;
     camera.position.z = cameraPosGame;
-    camera.position.y - 100;
+    camera.position.y = 30;
     camera.lookAt(new THREE.Vector3(0, 30, 0))
 
     renderer = new THREE.WebGLRenderer({
@@ -120,7 +118,6 @@ function createScene() {
     container.appendChild(renderer.domElement);
 
     window.addEventListener('resize', handleWindowResize, false);
-    document.addEventListener('mousemove', handleMouseMove, false);
     document.addEventListener('mousedown', handleMouseDown, false);
     //document.addEventListener("touchend", handleMouseDown, false);
     clock = new THREE.Clock();
@@ -133,10 +130,6 @@ function handleWindowResize() {
     renderer.setSize(width, height);
     camera.aspectRatio = width / height;
     camera.updateProjectionMatrix();
-}
-
-function handleMouseMove(event) {
-  mousePos = {x:event.clientX, y:event.clientY};
 }
 
 function handleMouseDown(event){
@@ -927,7 +920,7 @@ Ball.prototype.run = function() {
   this.runningCycle = this.runningCycle % (Math.PI*2);
   var t = this.runningCycle;
   //this.body.rotateZ(Math.sin(t) * Math.PI/4);
-  this.body.rotation.z += .1;
+  this.body.rotation.x += .1;
 }
 
 Ball.prototype.jump = function(){
@@ -1095,6 +1088,14 @@ function createWool(){
   wool.threeGroup.position.set(40,12,50);
 }
 
+function updateWoolPosition(){
+  wool.threeGroup.rotation.y += delta * 6;
+  wool.threeGroup.rotation.z = Math.PI/2 - (floorRotation+wool.angle);
+  wool.threeGroup.position.y = -floorRadius + Math.sin(floorRotation+wool.angle) * (floorRadius+50);
+  wool.threeGroup.position.x = Math.cos(floorRotation+wool.angle) * (floorRadius+50);
+  
+}
+
 BonusParticles = function(){
   this.mesh = new THREE.Group();
   var bigParticleGeom = new THREE.CubeGeometry(10,10,10,1);
@@ -1132,6 +1133,243 @@ function removeParticle(p){
   p.visible = false;
 }
 
+function createBonusParticles(){
+  bonusParticles = new BonusParticles();
+  bonusParticles.mesh.visible = false;
+  scene.add(bonusParticles.mesh);
+  
+}
+
+
+Hedgehog = function() {
+  this.angle = 0;
+  this.status="ready";
+  this.mesh = new THREE.Group();
+  var bodyGeom = new THREE.CubeGeometry(6,6,6,1);
+  this.body = new THREE.Mesh(bodyGeom, blackMat);
+  
+  var headGeom = new THREE.CubeGeometry(5,5,7,1);
+  this.head= new THREE.Mesh(headGeom, lightBrownMat);
+  this.head.position.z = 6;
+  this.head.position.y = -.5;
+  
+  var noseGeom = new THREE.CubeGeometry(1.5,1.5,1.5,1);
+  this.nose = new THREE.Mesh(noseGeom, blackMat);
+  this.nose.position.z = 4;
+  this.nose.position.y = 2;
+  
+  var eyeGeom = new THREE.CubeGeometry(1,3,3);
+  
+  this.eyeL = new THREE.Mesh(eyeGeom, whiteMat);
+  this.eyeL.position.x = 2.2;
+  this.eyeL.position.z = -.5;
+  this.eyeL.position.y = .8;
+  this.eyeL.castShadow = true;
+  this.head.add(this.eyeL);
+  
+  var irisGeom = new THREE.CubeGeometry(.5,1,1);
+  
+  this.iris = new THREE.Mesh(irisGeom, blackMat);
+  this.iris.position.x = .5;
+  this.iris.position.y = .8;
+  this.iris.position.z = .8;
+  this.eyeL.add(this.iris);
+  
+  this.eyeR = this.eyeL.clone();
+  this.eyeR.children[0].position.x = -this.iris.position.x;
+  this.eyeR.position.x = -this.eyeL.position.x;
+  
+  var spikeGeom = new THREE.CubeGeometry(.5,2,.5,1);
+  spikeGeom.applyMatrix(new THREE.Matrix4().makeTranslation(0,1,0));
+  
+  for (var i=0; i<9; i++){ 
+    var row = (i%3);
+    var col = Math.floor(i/3);
+    var sb = new THREE.Mesh(spikeGeom, blackMat);
+    sb.rotation.x =-Math.PI/2 + (Math.PI/12*row) -.5 +  Math.random();
+    sb.position.z = -3;
+    sb.position.y = -2 + row*2;
+    sb.position.x = -2 + col*2; 
+    this.body.add(sb); 
+    var st = new THREE.Mesh(spikeGeom, blackMat);
+    st.position.y = 3;
+    st.position.x = -2 + row*2;
+    st.position.z = -2 + col*2;
+    st.rotation.z = Math.PI/6 - (Math.PI/6*row) -.5 +  Math.random();
+    this.body.add(st);
+    
+    var sr = new THREE.Mesh(spikeGeom, blackMat);
+    sr.position.x = 3;
+    sr.position.y = -2 + row*2;
+    sr.position.z = -2 + col*2;
+    sr.rotation.z = -Math.PI/2 + (Math.PI/12*row) -.5 +  Math.random();
+    this.body.add(sr);
+    
+    var sl = new THREE.Mesh(spikeGeom, blackMat);
+    sl.position.x = -3;
+    sl.position.y = -2 + row*2;
+    sl.position.z = -2 + col*2;
+    sl.rotation.z = Math.PI/2  - (Math.PI/12*row) -.5 +  Math.random();;
+    this.body.add(sl); 
+  }
+
+  this.head.add(this.eyeR);
+  var earGeom = new THREE.CubeGeometry(2, 2, .5, 1);
+  this.earL = new THREE.Mesh(earGeom, lightBrownMat);
+  this.earL.position.x = 2.5;
+  this.earL.position.z = -2.5;
+  this.earL.position.y = 2.5;
+  this.earL.rotation.z = -Math.PI/12;
+  this.earL.castShadow = true;
+  this.head.add(this.earL);
+  
+  this.earR = this.earL.clone();
+  this.earR.position.x = -this.earL.position.x;
+  this.earR.rotation.z = -this.earL.rotation.z;
+  this.earR.castShadow = true;
+  this.head.add(this.earR);
+  
+  var mouthGeom = new THREE.CubeGeometry( 1, 1,.5, 1);
+  this.mouth = new THREE.Mesh(mouthGeom, blackMat);
+  this.mouth.position.z = 3.5;
+  this.mouth.position.y = -1.5;
+  this.head.add(this.mouth);
+  
+  
+  this.mesh.add(this.body);
+  this.body.add(this.head);
+  this.head.add(this.nose);
+
+  this.mesh.traverse(function(object) {
+    if (object instanceof THREE.Mesh) {
+      object.castShadow = true;
+      object.receiveShadow = true;
+    }
+  });
+}
+
+Hedgehog.prototype.nod = function(){
+  var _this = this;
+  var speed = .1 + Math.random()*.5;
+  var angle = -Math.PI/4 + Math.random()*Math.PI/2;
+  TweenMax.to(this.head.rotation, speed, {y:angle, onComplete:function(){
+    _this.nod();
+  }});
+}
+
+function createObstacle(){
+  obstacle = new Hedgehog();
+  obstacle.body.rotation.y = -Math.PI/2;
+  obstacle.mesh.scale.set(1.1,1.1,1.1);
+  obstacle.mesh.position.y = floorRadius+4;
+  obstacle.nod();
+  scene.add(obstacle.mesh);
+}
+
+function gameOver(){
+  fieldGameOver.className = "show";
+  gameStatus = "gameOver";
+  monster.sit();
+  //hero.hang();
+  //monster.heroHolder.add(hero.mesh);
+  TweenMax.to(this, 1, {speed:0});
+  TweenMax.to(camera.position, 3, {z:cameraPosGameOver, y: 60, x:-30});
+  wool.mesh.visible = false;
+  obstacle.mesh.visible = false;
+  clearInterval(levelInterval);
+}
+
+function replay(){
+  
+  gameStatus = "preparingToReplay"
+  
+  fieldGameOver.className = "";
+  
+  TweenMax.killTweensOf(monster.pawFL.position);
+  TweenMax.killTweensOf(monster.pawFR.position);
+  TweenMax.killTweensOf(monster.pawBL.position);
+  TweenMax.killTweensOf(monster.pawBR.position);
+  
+  TweenMax.killTweensOf(monster.pawFL.rotation);
+  TweenMax.killTweensOf(monster.pawFR.rotation);
+  TweenMax.killTweensOf(monster.pawBL.rotation);
+  TweenMax.killTweensOf(monster.pawBR.rotation);
+  
+  TweenMax.killTweensOf(monster.tail.rotation);
+  TweenMax.killTweensOf(monster.head.rotation);
+  TweenMax.killTweensOf(monster.eyeL.scale);
+  TweenMax.killTweensOf(monster.eyeR.scale);
+  
+  //TweenMax.killTweensOf(hero.head.rotation);
+  
+  monster.tail.rotation.y = 0;
+    
+  TweenMax.to(camera.position, 3, {z:cameraPosGame, x:0, y:30, ease:Power4.easeInOut});
+  TweenMax.to(monster.torso.rotation,2, {x:0, ease:Power4.easeInOut});
+  TweenMax.to(monster.torso.position,2, {y:0, ease:Power4.easeInOut});
+  TweenMax.to(monster.pawFL.rotation,2, {x:0, ease:Power4.easeInOut});
+  TweenMax.to(monster.pawFR.rotation,2, {x:0, ease:Power4.easeInOut});
+  TweenMax.to(monster.mouth.rotation,2, {x:.5, ease:Power4.easeInOut});
+  
+  
+  TweenMax.to(monster.head.rotation,2, {y:0, x:-.3, ease:Power4.easeInOut});
+  
+  TweenMax.to(hero.mesh.position, 2, { x:20, ease:Power4.easeInOut});
+  TweenMax.to(hero.head.rotation, 2, { x:0, y:0, ease:Power4.easeInOut});
+  TweenMax.to(monster.mouth.rotation, 2, {x:.2, ease:Power4.easeInOut});
+  TweenMax.to(monster.mouth.rotation, 1, {x:.4, ease:Power4.easeIn, delay: 1, onComplete:function(){
+    
+    resetGame();
+  }});
+  
+}
+
+function checkCollision(){
+  var db = ball.threeGroup.position.clone().sub(wool.threeGroup.position.clone());
+  //var dm = ball.threeGroup.position.clone().sub(obstacle.mesh.position.clone());
+  
+  if(db.length() < collisionBonus){
+    getBonus();
+  }
+  
+  //if(dm.length() < collisionObstacle && obstacle.status != "flying"){
+    //getMalus();
+  //}
+}
+
+function getBonus(){
+  bonusParticles.mesh.position.copy(wool.threeGroup.position);
+  bonusParticles.mesh.visible = true;
+  bonusParticles.explose();
+  wool.angle += Math.PI/2;
+  //speed*=.95;
+  monsterPosTarget += .025;
+  
+}
+
+function getMalus(){
+  obstacle.status="flying";
+  var tx = (Math.random()>.5)? -20-Math.random()*10 : 20+Math.random()*5;
+  TweenMax.to(obstacle.mesh.position, 4, {x:tx, y:Math.random()*50, z:350, ease:Power4.easeOut});
+  TweenMax.to(obstacle.mesh.rotation, 4, {x:Math.PI*3, z:Math.PI*3, y:Math.PI*6, ease:Power4.easeOut, onComplete:function(){
+    obstacle.status = "ready";
+    obstacle.body.rotation.y = Math.random() * Math.PI*2;
+    obstacle.angle = -floorRotation - Math.random()*.4;
+    
+    obstacle.angle = obstacle.angle%(Math.PI*2);
+    obstacle.mesh.rotation.x = 0;
+    obstacle.mesh.rotation.y = 0;
+    obstacle.mesh.rotation.z = 0;
+    obstacle.mesh.position.z = 0;
+    
+  }});
+  //
+  monsterPosTarget -= .04;
+  TweenMax.from(this, .5, {malusClearAlpha:.5, onUpdate:function(){
+    renderer.setClearColor(malusClearColor, malusClearAlpha );
+  }})
+}
+
 function updateFloorRotation(){
     floorRotation += delta*.03 * speed;
     floorRotation = floorRotation%(Math.PI*2);
@@ -1141,7 +1379,13 @@ function updateFloorRotation(){
 function updateDistance() {
   distance += delta * speed;
   var d = distance / 2;
-  //fieldDistance.inn
+  fieldDistance.innerHTML = Math.floor(d);
+}
+
+function updateLevel(){
+  if (speed >= maxSpeed) return;
+  level++;
+  speed += 2; 
 }
 
 function loop(){
@@ -1158,9 +1402,9 @@ function loop(){
       //ball.run();
       updateDistance();
       updateCatPosition();
-      //updateCarrotPosition();
+      updateWoolPosition();
       //updateObstaclePosition();
-      //checkCollision();
+      checkCollision();
     } 
     render();
     //var ballPos = getBallPos();
@@ -1186,28 +1430,31 @@ function init() {
     createCat();
     createBall();
     createWool();
+    createBonusParticles();
+    initUI();
+    resetGame();
     loop();
     
 }
 
 function resetGame(){
-  scene.add(hero.mesh);
-  hero.mesh.rotation.y = Math.PI/2;
-  hero.mesh.position.y = 0;
-  hero.mesh.position.z = 0;
-  hero.mesh.position.x = 0;
+  scene.add(ball.threeGroup);
+  ball.threeGroup.rotation.y = Math.PI/2;
+  ball.threeGroup.position.y = 6;
+  ball.threeGroup.position.z = 50;
+  ball.threeGroup.position.x = 10;
 
   monsterPos = .56;
   monsterPosTarget = .65;
   speed = initSpeed;
   level = 0;
   distance = 0;
-  carrot.mesh.visible = true;
-  obstacle.mesh.visible = true;
+  wool.threeGroup.visible = true;
+  //obstacle.mesh.visible = true;
   gameStatus = "play";
-  hero.status = "running";
-  hero.nod();
-  audio.play();
+  ball.status = "running";
+  //hero.nod();
+  //audio.play();
   updateLevel();
   levelInterval = setInterval(updateLevel, levelUpdateFreq);
 }
